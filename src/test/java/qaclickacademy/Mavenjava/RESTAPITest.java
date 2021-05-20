e759f9ac3642c1c8110941bd98c2ddfbf329a149
trupti <truptiRmagdum@gmail.com>
2021-05-20 18:55:23 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void posJira()
+	{
+		System.out.println("posJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+	System.out.println("deleteTwitter");	
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/abcTest.java
@@ -0,0 +1,13 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class abcTest {
+	
+	@Test
+	public void abc()
+	{
+		System.out.println("Hi");	
+	}
+
+}
